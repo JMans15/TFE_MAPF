4f581cbfa17f5a8613e4f233aba6dcff123290a8
JMans15 <mansjulien@gmail.com>
2022-09-29 13:05:41 +0200

    Added Qt5 support (suggestion)

--- library.cpp
+++ library.cpp
@@ -1,7 +1,10 @@
 #include "library.h"
 
 #include <iostream>
+#include <QApplication>
+#include <QDebug>
 
 void hello() {
     std::cout << "Hello, World!" << std::endl;
+    qDebug() << "Qt version :" << QT_VERSION_STR;
 }
