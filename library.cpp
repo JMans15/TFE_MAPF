75e9cc6bd3c1561eb7002aa119eb8bf7738936d2
JMans15 <mansjulien@gmail.com>
2022-10-15 09:43:44 +0200

    Revert "Added Qt5 support (suggestion)"
    This reverts commit 4f581cbfa17f5a8613e4f233aba6dcff123290a8.

--- library.cpp
+++ library.cpp
@@ -1,10 +1,7 @@
 #include "library.h"
 
 #include <iostream>
-#include <QApplication>
-#include <QDebug>
 
 void hello() {
     std::cout << "Hello, World!" << std::endl;
-    qDebug() << "Qt version :" << QT_VERSION_STR;
 }
