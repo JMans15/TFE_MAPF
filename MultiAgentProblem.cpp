436ca78f21306dfab61e6ac88c2c3e31d721f7e9
armahy <arthur.mahy@student.uclouvain.be>
2023-01-15 17:26:18 +0100

    Oops! Message for the previous commit: abstraction layer for the heuristics

---
+++ MultiAgentProblem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Arthur Mahy on 04/01/2023.
+//
+
+#include "MultiAgentProblem.h"
---
+++ MultiAgentProblem.h
@@ -0,0 +1,14 @@
+//
+// Created by Arthur Mahy on 04/01/2023.
+//
+
+#ifndef TFE_MAPF_MULTIAGENTPROBLEM_H
+#define TFE_MAPF_MULTIAGENTPROBLEM_H
+#include "Problem.h"
+
+class MultiAgentProblem : Problem{
+
+};
+
+
+#endif //TFE_MAPF_MULTIAGENTPROBLEM_H
