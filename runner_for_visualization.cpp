573e4aac59b151e93d0d570b6121ec1df803e73c
JMans15 <mansjulien@gmail.com>
2023-02-03 19:28:05 +0100

    Began adding multi agent support to runner

--- runner_for_visualization.cpp
+++ runner_for_visualization.cpp
@@ -11,46 +11,83 @@ int main(int argc, const char** argv) {
     cxxopts::Options options("TFE_MAPF_visu", "Runner program for visualizing our MAPF results");
 
     options.add_options()
-        ("f, file", "Path to file containing the map info", cxxopts::value<std::string>())
-        ("s, start", "Index of the starting position", cxxopts::value<int>())
-        ("t, target", "Index of the target position", cxxopts::value<int>())
+        ("map", "Path to file containing the map info", cxxopts::value<std::string>())
+        ("scen", "Path to file containing the scenario info", cxxopts::value<std::string>())
+        ("s, start", "Index of the starting position, only for single agent", cxxopts::value<int>())
+        ("t, target", "Index of the target position, only for single agent", cxxopts::value<int>())
         ("v, verbose", "Verbose output", cxxopts::value<bool>()->default_value("false"))
         ("h, help", "Print help")
+        ("m, multi", "Multi agent version")
         ;
 
     auto result = options.parse(argc, argv);
 
-    // region Checking for mandatory arguments
-    if (result.count("h")) {
-        std::cout << options.help({""}) << std::endl;
-        exit(0);
-    }
+    enum mode {
+        SINGLE,
+        MULTI
+    };
 
-    if (!result.count("f")) {
-        std::cout << "Arg file is mandatory" << std::endl;
-        exit(0);
-    }
+    mode Mode;
 
-    if (!result.count("s")) {
-        std::cout << "Arg start is mandatory" << std::endl;
-        exit(0);
+    // region arguments logic
+    if (result.count("m")) {
+        Mode = MULTI;
+        if (result.count("s") || result.count("t")) {
+            cout << "Cant't use s or t in multi agent mode" << endl;
+            exit(0);
+        }
+        if (!result.count("scen")) {
+            std::cout << "Arg scen is mandatory in multi agent mode" << std::endl;
+            exit(0);
+        }
     }
+    else {
+        Mode = SINGLE;
+        if (result.count("h")) {
+            std::cout << options.help({""}) << std::endl;
+            exit(0);
+        }
 
-    if (!result.count("t")) {
-        std::cout << "Arg target is mandatory" << std::endl;
+        if (!result.count("s")) {
+            std::cout << "Arg start is mandatory in single agent mode" << std::endl;
+            exit(0);
+        }
+
+        if (!result.count("t")) {
+            std::cout << "Arg target is mandatory in single agent mode" << std::endl;
+            exit(0);
+        }
+
+        if (result.count("scen")) {
+            std::cout << "Can't use arg scen in single agent mode" << std::endl;
+            exit(0);
+        }
+    }
+    if (!result.count("map")) {
+        std::cout << "Arg map is mandatory" << std::endl;
         exit(0);
     }
     // endregion
 
     // Getting filename and checking map file extension
-    auto file = result["f"].as<std::string>();
+    auto file = result["map"].as<std::string>();
     if (file.substr(file.find_last_of('.') + 1) != "map") {
         std::cout << "Not a map file" << std::endl;
         exit(0);
     }
 
+    auto scenfile = result["scen"].as<std::string>();
+    if (file.substr(file.find_last_of('.') + 1) != "scen") {
+        std::cout << "Not a scen file" << std::endl;
+        exit(0);
+    }
+
     // Parsing the graph from the map file
     Graph g = parser::parse(file);
+
+    if (Mode == MULTI) {
+        cout << "Multi agent currently not supported, WIP" << endl;
+    }
 
     // Getting start and target
     auto start = result["s"].as<int>();
