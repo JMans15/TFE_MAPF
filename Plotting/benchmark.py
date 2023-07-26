22818e39e4c0f3fc5a8d231f0f815b13b333100f
JMans15 <mansjulien@gmail.com>
2023-07-26 02:23:30 +0200

    Super giga benchmark.py

--- Plotting/benchmark.py
+++ Plotting/benchmark.py
@@ -1,61 +1,51 @@
 import os
+import numpy as np
 import subprocess
 import time
 from multiprocessing import Pool, current_process, freeze_support  # noqa: F401
 
-from matplotlib import pyplot as plt
-
 import utils
 
 program = "../cmake-build/TFE_MAPF_visu"
-num_threads = (
-    1  # Number of threads to use (do not use too much because of RAM usage of mapf)
-)
-tmax = 240
-nagents = 10
-
-algs = ["AStar", "SIDAStar", "ID", "CBS", "IDCAT", "DSCBS", "CBSCAT"]
-
-maps = [
-    "../mapf-map/32-32-1",
-    "../mapf-map/32-32-2",
-    "../mapf-map/32-32-3",
-    "../mapf-map/32-32-4",
-    "../mapf-map/32-32-5",
-    "../mapf-map/32-32-6",
-    "../mapf-map/Paris",
-]
-
-problems = []
-for mp in maps:
-    wlk = os.walk(mp)
-    tmp = wlk.__next__()
-    mapfile = tmp[2][0]
-    scendir = tmp[1][1]
-    _ = wlk.__next__()
-    scenfiles = wlk.__next__()[2]
-    for scenfile in scenfiles:
-        problems.append(
-            [os.path.join(mp, mapfile), os.path.join(mp, scendir, scenfile)]
-        )
-print(f"{len(problems)} problems found")
-
-errors = []
 
 
-def run_program(a, i, algo, to):
+def extract_problems(maps):
+    problems = []
+    for mp in maps:
+        wlk = os.walk(mp)
+        tmp = wlk.__next__()
+        mapfile = tmp[2][0]
+        tmp = wlk.__next__()
+        scendir = tmp[0]
+        scenfiles = tmp[2]
+        for scenfile in scenfiles:
+            problems.append(
+                [os.path.join(mp, mapfile), os.path.join(scendir, scenfile)]
+            )
+        tmp = wlk.__next__()
+        scendir = tmp[0]
+        scenfiles = tmp[2]
+        for scenfile in scenfiles:
+            problems.append(
+                [os.path.join(mp, mapfile), os.path.join(scendir, scenfile)]
+            )
+    return problems
+
+
+def run_and_check(a, i, algo, to, problem):
     try:
+        filename = f"../Plotting/result_{algo}_{i}_{a}.txt"
         ok = subprocess.run(
             [
                 program,
                 "--scen",
-                f"{problems[i][1]}",
+                f"{problem[1]}",
                 "--map",
-                f"{problems[i][0]}",
+                f"{problem[0]}",
                 "-n",
                 f"{a}",
                 "--outfile",
-                f"../Plotting/result_{algo}_{current_process().name}_{i}.txt",
+                filename,
                 "-a",
                 f"{algo}",
             ],
@@ -68,54 +58,75 @@ def run_program(a, i, algo, to):
             print("There was an error")
             return False
         # Check if solution is correct
-        with open(f"result_{algo}_{current_process().name}_{i}.txt", "r") as file:
+        with open(filename, "r") as file:
             if not utils.check_solution(file):
-                print(f"failed with scenario {problems[i][1]}")
-                os.remove(f"result_{algo}_{current_process().name}_{i}.txt")
+                print(f"failed with scenario {problem[1]}")
+                os.remove(filename)
                 return False
-            if not utils.check_start_and_target(file, problems[i][1], a):
-                print(f"failed with scenario {problems[i][1]}")
-                os.remove(f"result_{algo}_{current_process().name}_{i}.txt")
+            if not utils.check_start_and_target(file, problem[1], a):
+                print(f"failed with scenario {problem[1]}")
+                os.remove(filename)
                 return False
-        os.remove(f"result_{algo}_{current_process().name}_{i}.txt")
+        os.remove(filename)
         return True
     except subprocess.TimeoutExpired:
         return False
 
 
-def get_alg_perf(alg, timeout):
-    print(f"========== Starting with {alg}, {timeout} ==========")
+def get_alg_perf(alg, timeout, i, problem, nagents):
+    print(alg, nagents, problem[1])
     start = time.time()
-    nprob = len(problems)
-    elapsed = 0
-    timestamps = [0.0]
-    for i in range(nprob):
-        run_program(nagents, i, alg, timeout - elapsed)
-        now = time.time()
-        elapsed = now - start
-        if elapsed >= timeout:
-            return timestamps
-        # if (i + 1) % 5 == 0 or i + 1 == nprob:
-        timestamps.append(elapsed)
-        print(f"# {alg}: {i+1}/{nprob}")
-    return timestamps
+    ok = run_and_check(nagents, i, alg, timeout, problem)
+    now = time.time()
+    return (ok, now - start)
 
 
 if __name__ == "__main__":
     freeze_support()
-    # with Pool(num_threads) as p:
-    # results = [p.apply_async(get_alg_perf, (alg, tmax)) for alg in algs]
-    # for r in results:
-    #     r.wait()
-    # results = list(map(lambda x: list(x.get()), results))
-    results = [get_alg_perf(alg, tmax) for alg in algs]
+    nagents = np.arange(1, 51, 1, dtype=int)
+    tmax = 30
+    num_threads = 1
+    algs = [
+        "AStar",
+        "CBS",
+        "CBSCAT",
+        "Coop",
+        "ID",
+        "IDCAT",
+        "SID",
+        "SIDCAT",
+        "DSCBS",
+        "StandardAStar",
+        "EID",
+        "SIDAStar",
+        "SIDCBS",
+        "SIDCATAStar",
+        "SIDCATCBS",
+    ]
 
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    fig.suptitle(f"{nagents} agents, 175 problems")
-    ax.set_xlabel("Timout [s]")
-    ax.set_ylabel("Problems solved")
-    for i, a in enumerate(algs):
-        ax.plot(range(len(results[i])), results[i], label=a, linestyle="dashed")
-    ax.legend()
-    plt.show()
+    maps = [
+        "../mapf-map/32-32-1",
+        "../mapf-map/32-32-2",
+        "../mapf-map/32-32-3",
+        "../mapf-map/32-32-4",
+        "../mapf-map/32-32-5",
+        "../mapf-map/32-32-6",
+        "../mapf-map/Paris",
+    ]
+    problems = extract_problems(maps)
+    print(f"{len(problems)} problems found")
+    pool = Pool(num_threads)
+    results = np.array(
+        [
+            [
+                [
+                    pool.apply_async(get_alg_perf, args=(a, tmax, i, p, n))
+                    for n in nagents
+                ]
+                for a in algs
+            ]
+            for i, p in enumerate(problems)
+        ]
+    )
+    results = np.array([[[r.get() for r in b] for b in a] for a in results])
+    np.save("results.npy", results)
